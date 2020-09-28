6d803a69a0ce13310e10c40db4b3050a3e4dd88f
fosterv2 <fosterv2@outlook.com>
2020-09-28 12:16:58 -0700

    add dependencies for testing

--- test/testTree.js
+++ test/testTree.js
@@ -1,12 +1,20 @@
+require('../index')
 const expect = require('chai').expect
 
 describe('Trees', function () {
     describe('#makeNode()', function () {
-        it('makes a node when give a name, left, and right', function () {
-            expect(makeNode('A', {name: 'B'}, {name: 'C'})).to.equal({ name: 'A', left: { name: 'B' }, right: { name: 'C' } })
+        it('makes a node when given a data, left, and right', function () {
+            const B = { data: 'B' }
+            const C = { data: 'C' }
+            const newNode = makeNode('A', B, C)
+            expect(newNode.data).to.equal('A')
+            expect(newNode.left).to.equal(B)
+            expect(newNode.right).to.equal(C)
         })
         it('makes a node without left or right', function () {
-            expect(makeNode('A')).to.equal({ name: 'A', left: undefined, right: undefined })
+            const newNode = makeNode('A')
+            expect(newNode.data).to.equal('A')
+            expect(newNode.left, newNode.right).to.be.undefined
         })
     })
 })
